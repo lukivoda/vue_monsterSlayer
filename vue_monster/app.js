dab87029ce90e68b69fa1d9a70cfaee2905d06b3
Stevan Ristov <stevanris@gmail.com>
2017-11-23 17:35:05 +0100

    added attack function with helper functions

--- vue_monster/app.js
+++ vue_monster/app.js
@@ -18,6 +18,64 @@ new Vue({
             this.gameIsRunning = true;
             this.playerHealth = 100;
             this.monsterHealth = 100;
+        },
+
+        attack: function(){
+     //odzemame poeni od monster
+         this.monsterHealth -= this.calculateDamage(3,10);
+        // ako vracame true(ako player pobedil izleguvame od f-jata)
+        if(this.checkWin()){
+            return;
+        }
+
+            //odzemame poeni od player
+         this.playerHealth -= this.calculateDamage(5,10);
+            //ako monster pobedil izleguvame od f-jata(bidejci ova e posleden kod nema potreba od return)
+         this.checkWin();
+
+        },
+
+        specialAttack: function() {
+
+
+        },
+
+        heal:function() {
+
+
+        },
+
+        giveUp: function(){
+
+
+        },
+
+        //presmetuvame maximalen broj
+        calculateDamage: function(min,max){
+            return Math.max(min,Math.floor(Math.random()*max));
+        },
+
+        checkWin() {
+              //proveruvame koj pobedil vo igrata
+            // ako nekoj pobedil i klknal ok na prasanjeto igrata ja startirame odnovo,a  ako nekoj pobedil i stisnal cancel go pokazuvame samo start kopceto i vracame true posle logikata.Nadvor od logikata imame false
+            if(this.monsterHealth <= 0){
+                if(confirm("You won!New game?")){
+                    this.startGame();
+                }else{
+                    this.gameIsRunning = false;
+                }
+                return true;
+            } else if(this.playerHealth <=0){
+                if(confirm("Monster won!New game?")){
+                    this.startGame();
+                }else{
+                    this.gameIsRunning = false;
+                }
+                return true
+
+            }
+             return false;
+
         }
 
 
